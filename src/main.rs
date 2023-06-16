22e4ae47ed6466f9fcfd8a4752793b39bee512ef
megachonker <>
2023-06-16 22:42:50 +0200

    switch: fn client server and rdv

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,62 @@
 use clap::Parser;
-
+use std::net::IpAddr;
 #[derive(Parser)] // requires `derive` feature
-#[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Mode client: besoin addresse rendezvous 
-    #[arg(short,long)]
+    /// Mode client: besoin addresse rendezvous
+    #[arg(short, long)]
     client_flag: bool,
 
     /// Mode server: besoin addresse rendezvous
-    #[arg(short,long)]
+    #[arg(short, long)]
     server_flag: bool,
 
     /// Mode rendezvous: besoin de rien
-    #[arg(short,long)]
+    #[arg(short, long)]
     rdv_flag: bool,
 
     /// Addresse ip du point de rendez vous
-    rdv_address: Option<std::net::IpAddr>,
+    rdv_address: Option<IpAddr>,
 }
 
 fn main() {
     let args = Cli::parse();
+    let ip_rdv: IpAddr;
+
+    // do i have randezvous ?
+    if !args.rdv_flag {
+        match args.rdv_address {
+            Some(res) => ip_rdv = res,
+            None => {println!("Give IP address Please"); return;},
+        }
+        //I am client
+        if args.client_flag {
+            client(ip_rdv);
+        } 
+        //I am server
+        else if args.server_flag {
+            server(ip_rdv)
+        }
+        //I am nothing
+        else {
+            println!("no flag")
+        }
+    }
+    // I am randezvous 
+    else {
+        randezvous();
+    }
+}
+
+fn client(ip_rdv: IpAddr) {
+    println!("client");
+    println!("{:?}", ip_rdv);
+}
+
+fn server(ip_rdv: IpAddr) {
+    println!("server");
+    println!("{:?}", ip_rdv);
+}
+
+fn randezvous() {
+    println!("randezvous");
 }
